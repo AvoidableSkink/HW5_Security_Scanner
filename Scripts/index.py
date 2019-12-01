c15cf283af925b717e7e0e157b837b1c5eadffd5
Chris Merkley <09cmerkley1@gmail.com>
2019-12-01 14:54:54 -0700

    Making some changes to make the email checker more dynamic

--- Scripts/index.py
+++ Scripts/index.py
@@ -4,7 +4,7 @@ from collections import Counter
 from flask import Flask, render_template, request
 from difflib import get_close_matches
 
-from constants import TOP_500_DOMAINS
+from constants import TOP_500_DOMAINS, PHISHING_LIST
 
 app = Flask(__name__)
 
@@ -16,11 +16,11 @@ app = Flask(__name__)
 def task_one():
 
     if request.method == 'POST':
-        result = task_one(request.form['email'], request.form['email-body'])
-        return render_template('task1.html', prob=result[0], explanation=result[1])  # returns a tuple containing the prob of the email being a malicious email and the explanation why
+        prob, explanations = task_one(request.form['email'], request.form['email-body'])
+        return render_template('task1.html.j2', prob=prob, explanations=explanations, email=request.form['email'], body=request.form['email-body'])  # returns a tuple containing the prob of the email being a malicious email and a list of explanations why
 
     else:
-        return render_template('task1.html')
+        return render_template('task1.html.j2')
 
 
 @app.route('/task2', methods=['GET'])
@@ -56,72 +56,64 @@ def query_two_input():
 def task_one(senders_email, email_text):
     errors = []
 
-    spammy_word_list = ["label", "invoice", "post", "document", "postal", "calculations", "copy", "fedex", "statement",
-                        "financial", "dhl", "usps", "8", "notification", "irs", "ups", "no", "delivery", "ticket",
-                        "account", "secur", "verif", "com-", "update", "support", "service", "login", "Auth",
-                        "confirm", 'urgent', 'login', 'need', 'immediately', 'immediacy', 'right now', 'soon', 'expire', 'expiring',
-                        'as soon as possible', 'minutes', 'hours', 'seconds', 'days', 'months', 'urgency', 'account', 'bank', 'offer',
-                        'redeem', 'unusual', 'activity']
+    sus_o_meter = 0
+    contains_num = False
+    contains_spammy_words = False
+    contains_all_upper_words = False
+    contains_dollar_signs = False
+    spammy_domain = False
 
     email_text_list = email_text.split()  # split the email text into an array of strings
     split_email = senders_email.split('@')  # split the email name from the domain name
     email_name = split_email[0]
     email_domain = split_email[1]
 
-    score, error = domain_spam_score(test_domain=email_domain)
+    domain_score, error = domain_spam_score(test_domain=email_domain)
 
-    errors.append(error)
+    if error is not None:
+        errors.append(error)
 
-    sus_o_meter = score
-    contains_num = False
-    contains_spammy_words = False
-    spammy_domain = False
-
-    if email_domain in spammy_email_domains: # add 10 to the sus meter if the domain is in our spammy domain list
+    if domain_score > 10:
         spammy_domain = True
-        sus_o_meter += 10
+    sus_o_meter += domain_score
 
-    for word in spammy_word_list:  # add 1 to the sus meter if it contains a spammy word
-        if word in email_text:
+    for word in email_text_list:
+        if any([spam_word in word.lower() for spam_word in PHISHING_LIST]): #  This guarantees that we will catch partial words.
+            if not contains_spammy_words:
+                errors.append('Email contains words which are common in phishing attacks')
             contains_spammy_words = True
             sus_o_meter += 5
+        if word.isalpha() and word == word.upper():
+            if not contains_all_upper_words:
+                errors.append('Email contains words in all upper')
+            contains_all_upper_words = True
+            sus_o_meter += 1
+        if '$' in word:
+            if not contains_dollar_signs:
+                errors.append('Email contains dollar signs')
+            contains_dollar_signs = True
+            sus_o_meter += 1
 
     if contains_number(senders_email):  # add  3 to the sus meter if the email name contains a number
         contains_num = True
+        errors.append('Email contains a number')
         sus_o_meter += 3
 
 # ----- Range check and explanations -----
+    if sus_o_meter > 100:
+        sus_o_meter = 100
 
-    if sus_o_meter <= 3:
-        prob = sus_o_meter
-        explanation = 'Most likely a safe sender.'
+    if sus_o_meter <= 10:
+        return sus_o_meter, errors
 
-        return prob, explanation
-
-    if sus_o_meter > 3 and sus_o_meter <= 5:
+    if sus_o_meter > 10 and sus_o_meter <= 20:
         if contains_spammy_words == True:
-            prob = sus_o_meter
-            explanation = 'This may be a suspicious sender as it contains spammy words'
-            return prob, explanation
+            return sus_o_meter, errors
 
-    if sus_o_meter > 5:
-        if contains_spammy_words is True and spammy_domain is True:
-            if sus_o_meter > 10:
-                prob = 10
-            else:
-                prob = sus_o_meter
-            explanation = 'This is most likely a phishing attack as it contains a spammy email and spammy words'
-            return prob, explanation
-        elif contains_spammy_words is True and spammy_domain is False:
-            prob = sus_o_meter
-            explanation = 'This is most likely a phishing attack as it contains lots of spammy words'
-            return prob, explanation
-        else:
-            prob = sus_o_meter
-            explanation = 'This is most likely a phishing attack as it contains a spammy email domain'
-            return prob, explanation
+    if sus_o_meter > 20:
+        return sus_o_meter, errors
 
-    return 'error', 'error'
+    return 100, 'error'
 
 
 # Tautologies,
@@ -165,12 +157,12 @@ def contains_number(s):
 
 def domain_spam_score(test_domain=None):
     if test_domain is None:
-        return 100
+        return 50, 'No domain inserted'
     test_domain = test_domain.lower()
 
     #  Domain is in top_500_domains, not spam as far as domain is concerned
     if test_domain in TOP_500_DOMAINS:
-        return 0
+        return 0, None
 
     #  Check if domain has a spammy prefix/suffix
     for prefix, domain in product(['update', 'login', 'verify'], TOP_500_DOMAINS):
@@ -191,7 +183,7 @@ def domain_spam_score(test_domain=None):
         score = 50
     if len(get_close_matches(test_domain, TOP_500_DOMAINS, cutoff=0.75)) > 0:
         return score, 'Domain is not in list of known domains, but is close to the domain(s) {}'.format(get_close_matches(test_domain, TOP_500_DOMAINS, cutoff=0.75))
-    return score + 5, 'Domain is not in list of known domains'
+    return score + 5, 'Domain is not in list of known domains, be careful what kind of information you send to this address'
 
 
 if __name__ == '__main__':
